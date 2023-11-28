9169ccc6f93a998a2ceaa27dc51e4903c4716272
Obimbo07 <obimboausts@gmail.com>
2023-11-28 16:23:02 +0300

    navigation

---
+++ client/src/components/pages/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    { id: 1, text: 'Home', url: '/' },
+    { id: 2, text: 'Our Work', url: '/ourwork' },
+    { id: 3, text: 'Themes', url: '/themes', dropdown: ['Transport', 'Trainings', 'Seminars'] },
+    { id: 4, text: 'Media', url: '/media' },
+  ];
+
+  return (
+    <nav>
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            {link.dropdown ? (
+              <div className="dropdown">
+                <Link to={link.url}>{link.text}</Link>
+                <ul className="dropdown-content">
+                  {link.dropdown.map((item, index) => (
+                    <li key={index}>
+                      <Link to={`${link.url}/${item.toLowerCase().replace(/\s+/g, '-')}`}>{item}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <Link to={link.url}>{link.text}</Link>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
