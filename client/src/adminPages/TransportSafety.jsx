0e44d5e502c5bb53773014411a4cb7999cdb9dca
gerismumo <geraldmumo6@gmail.com>
2023-11-30 14:44:51 +0300

    image convert

--- client/src/adminPages/TransportSafety.jsx
+++ client/src/adminPages/TransportSafety.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useCallback, useEffect, useState } from 'react';
+import './Pages.css';
 
 const TransportSafety = () => {
 
@@ -26,7 +27,52 @@ const TransportSafety = () => {
     }, [selectTrans]);
     return (
         <div className="transport-page">
-            <h1>Transport page</h1>
+            <div className="title">
+                <h1>Transport Content</h1>
+            </div>
+            <div className="table">
+                <table>
+                    <thead>
+                        <th>Category</th>
+                        <th>Tags</th>
+                        <th>Title</th>
+                        <th>Content</th>
+                        <th>Image</th>
+                        <th>Post Date</th>
+                        <th>View More</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </thead>
+                    <tbody>
+                        {transportList.length === 0 ? (
+                            <tr>
+                                 <td>No Data</td>
+                            </tr>  
+                        ): (
+                            <>
+                            {transportList.map((data) => (
+                                <tr key={data.id}>
+                                    <td>{data.post_category}</td>
+                                    <td>{data.post_tag}</td>
+                                    <td>{data.post_title}</td>
+                                    <td>{data.post_content}</td>
+                                    <td>
+                                    <a href={URL.createObjectURL(new Blob([new Uint8Array(data.post_image.data)],{type: 'image/jpeg', }))}
+                                        download={URL.createObjectURL(new Blob([new Uint8Array(data.post_image.data)],{type: 'image/jpeg', }))}>
+                                            Download
+                                        </a>
+                                    </td>
+                                    <td>{data.date}</td>
+                                    <td><button>View More</button></td>
+                                    <td><button>Edit</button></td>
+                                    <td><button>Delete</button></td>
+                                </tr>
+                            ))}
+                            </>
+                        )} 
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
