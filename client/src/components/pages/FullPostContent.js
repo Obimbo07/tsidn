4197d9987afa485f96fb07de1e827a181cb40181
Obimbo07 <obimboausts@gmail.com>
2023-12-31 20:07:23 +0300

    fix-redux

--- client/src/components/pages/FullPostContent.js
+++ client/src/components/pages/FullPostContent.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import '../styles/FullPostContent.css';
 
 const FullPostContent = ({ blogPosts }) => {
   const { id } = useParams();
-  console.log(id);
-  console.log(blogPosts)
-  // Find the blog post with the matching id
 
   const blogPost = blogPosts.filter((post) => (
     post.id === parseInt(id)
@@ -14,7 +12,7 @@ const FullPostContent = ({ blogPosts }) => {
   const currentPost = blogPost[0]
   
   if (!currentPost) {
-    return <div>Blog post not found</div>;
+    return <div className='err-content'>Blog Content Doesn't Exist</div>;
   }
 
   
@@ -22,18 +20,10 @@ const FullPostContent = ({ blogPosts }) => {
 
   return (
     <div className="full-post-content">
-      <div >
+      <div className="blog-content" >
       <h2>{currentPost.title}</h2>
       <img src={currentPost.image} alt={currentPost.title} className="post-image" />
       <p>{currentPost.content}</p></div>
-      {/* <div className="full-post-content">
-     {blogPosts.map((blog) => (
-      <div keys={blog.id}>
-      <h2>{blog.title}</h2>
-      <img src={blog.image} alt={blog.title} className="post-image" />
-      <p>{blog.content}</p></div>
-     ))};
-    </div> */}
     </div>
    
   );
