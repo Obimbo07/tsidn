6b512b9b369092c6739a7d69c8bd0d5bb1642c62
Obimbo07 <obimboausts@gmail.com>
2024-01-14 21:33:58 +0300

    directors

--- client/src/components/pages/ExecutiveDirector.js
+++ client/src/components/pages/ExecutiveDirector.js
@@ -4,16 +4,29 @@ import '../styles/ExecutiveDirector.css';
 const ExecutiveDirector = () => {
   return (
     <div className="executive-director-container">
-      <img  alt="Executive Director" className="executive-director-image" />
+      <div className="intro-ed">
+      <img  alt="Executive Director" src="/ED.jpg" className="executive-director-image" />
+      <span>Executive Director</span>
+      </div>
+
       <div className="executive-director-text">
-        <h2>Title: Empowering Road Transport Safety and Development in Kenya: TSID-NET’s Mission</h2>
         <p>
-          Dear Friends and Supporters,
-          <br />
-          I am honored to share with you the vital work that the Transposafety International Development Network (TSID-NET) is undertaking to transform road transport safety and development in Kenya. As the Executive Director of TSID-NET, I am deeply committed to our mission, which revolves around fostering collaboration among key stakeholders at national, regional, and international levels. Our primary aim is to enhance the safety and development of the transport sector, ensuring the well-being of communities, the efficient movement of goods and services, and the overall growth of Kenya’s economy.
+          The Executive director helps through the organization to transform road transport safety and development in Kenya.I am deeply committed to our mission, 
+           which revolves around fostering collaboration among key stakeholders at national, regional, and 
+           international levels. Our primary aim is to enhance the safety and development of the transport sector, 
+           ensuring the well-being of communities, the efficient movement of goods and services, 
+           and the overall growth of Kenya’s economy.
         </p>
         <p>
-          TSID-NET is dedicated to improving road transport safety through various means. We provide expert knowledge sharing sessions, conduct training programs, and host national and regional road transport and safety technical seminars and conferences. Our quarterly magazine serves as a valuable resource for the latest updates and insights in the field. We also work tirelessly to advocate for good governance in transport safety and development, catalyze financing for safety improvement programs, and support grassroots awareness activities within communities. Our vision is to lead in the exchange of knowledge regarding safe and sustainable road transport and safety policy and practice. With your continued support, we can achieve these goals and ensure a safer, more sustainable future for road transport in Kenya.
+          TSID-NET is dedicated to improving road transport safety through various means.
+           We provide expert knowledge sharing sessions, conduct training programs, 
+           and host national and regional road transport and safety technical seminars and conferences. 
+           Our quarterly magazine serves as a valuable resource for the latest updates and insights in the field.
+            We also work tirelessly to advocate for good governance in transport safety and development, 
+            catalyze financing for safety improvement programs, and support grassroots awareness activities within communities. 
+             Our vision is to lead in the exchange of knowledge regarding safe and sustainable road transport
+              and safety policy and practice. 
+              With your continued support, we can achieve these goals and ensure a safer, more sustainable future for road transport in Kenya.
         </p>
         <p>
           Twitter | Linkedin | Email
