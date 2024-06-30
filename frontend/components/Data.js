a63730ef76de02f845fc96f1e87b4c536ef8533f
Obimbo07 <obimboausts@gmail.com>
2024-06-30 22:56:17 +0300

    Feature: 📲 created a data.js to contain  site data

---
+++ frontend/components/Data.js
@@ -0,0 +1,95 @@
+export const cardsData = [
+    {
+      id: 1,
+      title: 'Transport safety and development ',
+      iconUrl: 'camera_1972954.png',
+      path: 'themes/transport-safety-and-development'
+       
+    },
+    {
+      id: 2,
+      title: 'Expert knowledge sharing',
+      iconUrl: 'agreement_10476721.png',
+      path: 'themes/expert-knowledge-sharing-in-transport-safety-development'
+    },
+    {
+      id: 3,
+      title: 'Trainings',
+      iconUrl: '/traffic-cone.png',
+      path: 'themes/knowlegde-sharing'
+    },
+    {
+      id: 4,
+      title: 'Eco-Transport',
+      iconUrl: '/transportation_4515717.png',
+      path: 'themes/eco-transport-development'
+    },
+    {
+      id: 5,
+      title: 'Research and Development',
+      iconUrl: 'conedevelopment.png',
+      path: 'themes/research-and-development-in-transport'
+    },
+    {
+      id: 6,
+      title: 'Capacity Building',
+      iconUrl: 'supply-chain-management_11562996.png',
+      path: 'themes/capacity-building-and-human-resource-development-in-transport'
+    },
+];
+
+
+export const pressReleases = [
+    {   id: 1, 
+        title: 'news 2', 
+        content: 'Sample Dataset for the press releases with released or scheduled date.', 
+        Date: '25th Nov 2023' 
+    },
+
+    { id: 2, 
+      title: 'news 3', 
+      content: 'Sample Dataset for the press releases with released or scheduled date.', 
+      Date: '23rd Dec 2023' 
+    },
+
+    { id: 3, 
+      title: 'news 4', 
+      content: 'Sample Dataset for the press releases with released or scheduled date.', 
+      Date: '23rd Dec 2023' 
+    },
+];
+  
+export const podcasts = [
+    { id: 1, 
+      title: 'Fundamentals of road safety', 
+      date: '23rd Dec 2023 - 20th Dec 2023' 
+    },
+
+    { id: 2, 
+      title: 'Policy and regulatory framework in transport sector', 
+      date: '23rd Dec 2023 - 20th Dec 2023' 
+    },
+
+    { id: 3, 
+      title: 'Managing road safety risks', 
+      date: '23rd Dec 2023 - 20th Dec 2023' 
+    },
+
+    { id: 4, 
+      title: 'Speed management', 
+      date: '23rd Dec 2023 - 20th Dec 2023' 
+    },
+    { id: 5, 
+      title: 'Fatigue management', 
+      date: '23rd Dec 2023 - 20th Dec 2023' 
+    },
+    { id: 6, 
+      title: 'Journey planning', 
+      date: '23rd Dec 2023 - 20th Dec 2023' 
+    },
+    { 
+      id: 7, 
+      title: 'Drunk/drug driving', 
+      date: '23rd Dec 2023 - 20th Dec 2023' 
+    }
+];
