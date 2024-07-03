08970b2a35a552ce0932804c76eae51012b7ada8
Obimbo07 <obimboausts@gmail.com>
2024-07-03 19:53:53 +0300

    Feature: 🚀 added the featured video to home section

--- frontend/app/(root)/page.js
+++ frontend/app/(root)/page.js
@@ -11,6 +11,24 @@ export default function Home() {
       <div className="w-full md:flex md:justify-center">
         <PressRelease/>
         <Podcasts />
+        <div className="lower-section">
+          <div className="featured-videos">
+            <h3>FEATURED VIDEOS</h3>
+            <div className="video-container">
+
+              <iframe
+                width="460"
+                height="315"
+                src="https://www.youtube.com/embed/T--aotYDL0g?si=GPC24-McY4EBB1uJ"
+                title="YouTube Video 2"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        </div>
+        
       </div>
       
     </main>
