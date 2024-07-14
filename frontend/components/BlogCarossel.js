a7f3cf8f8197f91a4304c8cb44d0a5aa734ec085
Obimbo07 <obimboausts@gmail.com>
2024-07-14 21:38:00 +0300

    Feature: dispatched events data to component

--- frontend/components/BlogCarossel.js
+++ frontend/components/BlogCarossel.js
@@ -1,5 +1,3 @@
-'use client';
-
 import EmblaCarousel from './EmblaCarousel';
 import '../component_css/base.css'
 import '../component_css/embla.css'
@@ -14,55 +12,90 @@ const OPTIONS = { axis: 'x', align: 'start', dragFree: true, Autoplay: true }
 const SLIDES = [
     {
         title: 'Post 1',
-        img: 'shirley.jpg'
+        img: '/shirley.jpg'
     },
     {
         title: 'Post 2',
-        img: 'shirley.jpg'
+        img: '/shirley.jpg'
     },
     {
         title: 'Post 3',
-        img: 'shirley.jpg'
+        img: '/shirley.jpg'
     },
     {
         title: 'Post 4',
-        img: 'shirley.jpg'
+        img: '/shirley.jpg'
     },
     {
         title: 'Post 5',
-        img: 'shirley.jpg'
+        img: '/shirley.jpg'
     }
 ];
 
-const Events = [
-  {  img: 'LogoFinal.svg', title: '2 DAY EVENT AT CITAM', date: '14th - 20th January, Nairobi', content: 'Company writeup that is displayed on each card and listed from the api data.'},
-  {  img: 'LogoFinal.svg', title: 'ROAD SAFETY TRAINING TO SCHOOL DRIVERS', date: '20th - 22th January 2023, ', content: 'Company writeup that is displayed on each card and listed from the api data.' },
-  {  img: 'LogoFinal.svg', title: 'APPLICATION OF DRIVERS LISENCE AND RENEWALS', date: '25th - 20th January', content: 'Company writeup that is displayed on each card and listed from the api data.' },
-  {  img: 'LogoFinal.svg', title: 'MOMBASA ECO-TRANSPORT LAUNCH', date: '14th - 20th February', content: 'Company writeup that is displayed on each card and listed from the api data.' },
-]
 
 
-export function MajorEvents() {
+// const Events = [
+//   {  img: 'LogoFinal.svg', title: '2 DAY EVENT AT CITAM', date: '14th - 20th January, Nairobi', content: 'Company writeup that is displayed on each card and listed from the api data.'},
+//   {  img: 'LogoFinal.svg', title: 'ROAD SAFETY TRAINING TO SCHOOL DRIVERS', date: '20th - 22th January 2023, ', content: 'Company writeup that is displayed on each card and listed from the api data.' },
+//   {  img: 'LogoFinal.svg', title: 'APPLICATION OF DRIVERS LISENCE AND RENEWALS', date: '25th - 20th January', content: 'Company writeup that is displayed on each card and listed from the api data.' },
+//   {  img: 'LogoFinal.svg', title: 'MOMBASA ECO-TRANSPORT LAUNCH', date: '14th - 20th February', content: 'Company writeup that is displayed on each card and listed from the api data.' },
+// ]
+
+async function getMeetings() {
+    'use server'
+    const res = await fetch( `${process.env.REACT_APP_DATABASE_API}/api/selectMeeting`);
+
+  
+    if (!res.ok) {
+      throw new Error('Failed to fetch Meetings');
+    }
+  
+    const result = await res.json();
+    console.log(result.data)
+    return result.data;
+}
+
+// async function getNews() {
+//     'use server'
+//     const res = await fetch( `${process.env.REACT_APP_DATABASE_API}/api/selectNews`);
+//     console.log(res)
+  
+//     if (!res.ok) {
+//       throw new Error('Failed to fetch News');
+//     }
+  
+//     const News = await res.json();
+//     return News.data;
+// }
+
+export async function MajorEvents() {
+    const Events = await getMeetings();
+    console.log(Events)
     return (
-        <div className="flex flex-col h-auto mx-4 shadow-full my-2">
+        <div className="m-4 flex flex-col h-auto mx-4  shadow-full my-2">
          <h2 className="underline decoration-blue-700 decoration-width-full decoration-2 underline-offset-4">MAJOR EVENTS</h2>
+           <div className='divide-y divide-slate-400'>
             {Events.map((event) => (
-                <div className="flex text-neutral-900 my-4 flex-row" key={event.index} >
-                  <span className="text-2xl">
-                    <p className="text-blue-600">{event.title}</p>
-                    <p>{event.date}</p>
-                  </span>
-                  <img className="h-auto w-52" src={event.img} alt="tsdn logo svg" />
-                </div>
-            ))}
+                    <div className="m-2 flex text-neutral-900 my-4 flex-row" key={event.index} >
+                    <span className="text-2xl">
+                        <p className="text-blue-600">{event.post_title}</p>
+                        <span>{event.start_date} to {event.end_date} </span>
+                    </span>
+                    <img className="h-auto w-52" src={'/LogoFinal.svg'} alt="tsdn logo svg" />
+                    </div>
+                ))}
+           </div>
+            
         </div>        
     );
 }
 
-export default function BlogCarossel(){
+export default  function BlogCarossel(){
     return (
         <>
-            <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+            <div id="hero" >
+              <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+            </div>
         </>
     )
 }
