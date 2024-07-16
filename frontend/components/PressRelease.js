efcf29b749e5bd3096e15632e8acd95304db468a
Obimbo07 <obimboausts@gmail.com>
2024-07-16 05:18:58 +0300

    Fix: homepage carosssel colors

--- frontend/components/PressRelease.js
+++ frontend/components/PressRelease.js
@@ -5,12 +5,12 @@ import { pressReleases } from "./Data";
 const PressRelease = () => {
   return (
     <>
-   
-    <div className="">
-      <h2 className="font-mono underline text-3xl text-extrabold">Press release</h2>
+    <h2 className="font-mono underline underline-offset-8 decoration-sky-500 text-3xl mr-8 text-extrabold">Press release</h2>
+    <div className="m-4 divide-y-2 shadow-2xl shadow-inner ">
+      
       {pressReleases.map((release) => (
-        <div key={release.id}>
-          <p>{release.title}</p>
+        <div className=" bg-gradient-to-tr text-yellow-500 from-neutral-900 to-neutral-600   p-2" key={release.id}>
+          <h2 className='font-bold text-2xl '>{release.title}</h2>
           <p>{release.content}</p>
           <p>{release.Date}</p>
         </div>
