f3e014fcd0e2c8f4cb875686402d95bd947cc36a
Obimbo07 <obimboausts@gmail.com>
2024-07-26 18:57:58 +0300

    Feature: updated navigation bar design

---
+++ frontend/components/MobileNav.jsx
@@ -0,0 +1,68 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import Link from "next/link";
+import { useState } from "react";
+
+const MobileNav = ({ links }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+
+    return (
+        <>
+            <Sheet isOpen={isOpen} onOpenChange={toggleMenu}>
+                <SheetTrigger asChild>
+                    <button className="md:hidden" onClick={toggleMenu}>
+                        {isOpen ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        )}
+                    </button>
+                </SheetTrigger>
+
+                <SheetContent>
+                    <nav className={`bg-gray-100 p-4`}>
+                        <ul className="flex flex-col md:flex-row md:gap-4">
+                            {links.map((link) => (
+                                <li key={link.id} className="relative group">
+                                    <div className="flex items-center">
+                                        <Link href={link.url} className="block font-bold px-4 py-2">
+                                            {link.text}
+                                        </Link>
+                                        <span className="ml-2 font-bold md:hidden text-white bg-blue-500 rounded-full w-6 h-6 flex items-center justify-center">
+                                            +
+                                        </span>
+                                    </div>
+                                    <div className="w-max hidden md:absolute z-50 group-hover:block bg-white shadow-lg rounded">
+                                        <ul>
+                                            {link.dropdown.map((item) => (
+                                                <li key={item.id} className="px-4 py-2 hover:bg-gray-200">
+                                                    <Link href={`${link.url}${item.url}`}>
+                                                        {item.text}
+                                                    </Link>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </SheetContent>
+            </Sheet>
+        </>
+    )
+}
+
+export default MobileNav
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -2,11 +2,11 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-import Search from "./Search";
+import TopBadge from "./TopBadge";
+import SheetSide from "./SearchSheet";
+import MobileNav from "./MobileNav";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
 
   const links = [
     {
@@ -70,34 +70,31 @@ function Navbar() {
 
   return (
     <>
-      <div className='w-full flex'>
-          <ul className="md:flex flex w-full justify-center flex-center center gap-4">
+      <div className='w-full flex-col'>
+          <ul className="md:flex flex w-full justify-center  bg-blue-500 flex-center center gap-4">
               {toplinks.map((link) => (
                 <li key={link.id}>
-                  <Link className="hover:text-yellow-500 p-2" href={link.url}>{link.text}</Link>
+                  <Link className="text-white hover:text-gold-700 p-2" href={link.url}>{link.text}</Link>
                 </li>
               ))}
             </ul>
+            <TopBadge />
       </div>
       <nav className="flex-col  items-center p-4 bg-white shadow-md">
         <div className='flex items-center justify-between'>
             <Link className='' href={"/"}>
               <Image src="/LogoFinal.svg" alt="Logo" width={150} height={10} priority />
             </Link>
-           <Search />
+
+
+            <SheetSide />
+
+
+            
+            <MobileNav  links={links}/>
         </div>
 
-        <button className="hidden flex-end" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
-          )}
-        </button>
+        
 
         <div className="w-full">
             
@@ -107,31 +104,7 @@ function Navbar() {
        
       
 
-      <nav className={`md:flex ${isOpen ? "block" : "hidden"} justify-around bg-gray-100 p-4`}>
-        <ul className="flex flex-col md:flex-row md:gap-4">
-          {links.map((link) => (
-            <li key={link.id} className="relative group">
-              <div className="flex items-center">
-                <Link href={link.url} className="block font-bold px-4 py-2">
-                  {link.text}
-                </Link>
-                <span className="ml-2 font-bold md:hidden text-white bg-blue-500 rounded-full w-6 h-6 flex items-center justify-center ">+</span>
-              </div>
-              <div className="w-max hidden md:absolute z-50 group-hover:block bg-white shadow-lg rounded">
-                <ul>
-                  {link.dropdown.map((item) => (
-                    <li key={item.id} className="px-4 py-2 hover:bg-gray-200">
-                      <Link href={`${link.url}${item.url}`}>
-                        {item.text}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      
     </>
   );
 }
